478d982353dd948b5ae392875448fa6df2245daa
Vhmit <viktor.9630@protonmail.com>
2025-06-29 19:59:24 -0300

    extras: initial commit for packages
    Change-Id: I327e60551adae2829580657b76cd945a31f64f9f

---
+++ main.mk
@@ -0,0 +1,11 @@
+#
+# Copyright (C) 2025 The Android Open Source Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_SOONG_NAMESPACES += $(LOCAL_PATH)
+
+# Remove unwanted packages
+PRODUCT_PACKAGES += \
+    RemovePackages
