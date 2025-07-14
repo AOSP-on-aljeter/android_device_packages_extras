0df9921a5debc4fc71396259654da5102723e93e
Vhmit <viktor.9630@protonmail.com>
2025-07-14 10:36:02 -0300

    extras: Set adb properties for logs
    Change-Id: I6de56c4e18d52d140fa853e0ec3ce79309e6be5a

--- main.mk
+++ main.mk
@@ -14,3 +14,14 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     CamX \
     Via
+
+# ADB properties
+ifeq ($(TARGET_ADB_LOGS),true)
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.adb.secure=0 \
+    ro.secure=0 \
+    ro.debuggable=1 \
+    persist.sys.usb.config=adb \
+    persist.service.adb.enable=1 \
+    persist.service.debuggable=1
+endif
